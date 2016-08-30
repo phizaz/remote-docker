0f6399d53660d3b3fa1be90e02ca36fca1714a5d
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-08-30 23:36:52 +0700

    adding structure files

--- src/remotedocker.py
+++ src/remotedocker.py
@@ -1,8 +1,34 @@
+def act_list(args):
+    pass
+
+def act_set_default_host(args):
+    pass
+
+def act_new_host(args):
+    pass
+
+def act_new_alias(args):
+    pass
+
+def act_run(args):
+    pass
+
 def main():
     import sys
-    from src.parser import parseargs
+    from src.parser import parseargs, Actions
     args = parseargs(sys.argv[1:])
     print(args)
+
+    func_map = {
+        Actions.LIST: act_list,
+        Actions.DEFAULT: act_set_default_host,
+        Actions.NEW_HOST: act_new_host,
+        Actions.NEW_ALIAS: act_new_alias,
+        Actions.RUN: act_run
+    }
+
+    # call function 
+    func_map[args.action](args)
 
 if __name__ == '__main__':
     main()
