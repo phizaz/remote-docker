b306f54f93e1c9893dc7119f03881b4b1c468cb2
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-08-31 16:40:37 +0700

    implementing rsync; and updating utils and run

--- test/test_rsync.py
+++ test/test_rsync.py
@@ -19,4 +19,13 @@ class RsyncTest(unittest.TestCase):
     def test_rsync_up(self):
         utils.init_ignore()
         rsync.rsync_up('ta@desktop.dyn.konpat.me', '$(pwd)/Projects/test-remotedocker')
+        from os import remove
+        remove(utils.path_file_ignore())
+
+    def test_rsync_down(self):
+        utils.init_ignore()
+        rsync.rsync_down('ta@desktop.dyn.konpat.me', '$(pwd)/Projects/test-remotedocker')
+        from os import remove
+        remove(utils.path_file_ignore())
+
 
